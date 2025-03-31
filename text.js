e14022c3c92fa9fcda064b75c2c511941839014f
Alejandro Agüero <140672208+AlejandroAgueroDev@users.noreply.github.com>
2025-03-31 19:54:56 -0300

    Create text.js

---
+++ text.js
@@ -0,0 +1,4 @@
+//! Aca la idea es que escriban su nombre y una pequeña funcion para recorrer el siguiente array y mostrarlo por consola
+//! Luego hagan un git push origin 'NombreDeSuRama'
+//! No se olviden de fijarse en que rama estan al comienzo, recuerden que tienen que estar en sus ramas personales antes de hacer alguna modificacion
+const numeros=[1,2,3,4]
